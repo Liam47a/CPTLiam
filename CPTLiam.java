9e96e29807f540250769cdeb58aebd8c7e40a59a
Liam47a <Liam.JoycePlante26@ycdsbk12.ca>
2025-05-23 09:32:46 -0400

    Update CPTLiam.java

--- CPTLiam.java
+++ CPTLiam.java
@@ -3,6 +3,11 @@ import arc.*;
 public class CPTLiam {
 	
 	public static void main (String[] args) {
+		String strCard1 = "";
+		String strCard2 = "";
+		String strCard3 = "";
+		String strCard4 = "";
+		String strCard5 = "";
 		Console con = new Console();
 		con.println("Hello World!");
 	}
