0dd11d6eb806473fdee537af025d37bf7abed85a
Liam47a <Liam.JoycePlante26@ycdsbk12.ca>
2025-05-23 09:43:04 -0400

    Some comments

--- CPTLiam.java
+++ CPTLiam.java
@@ -8,6 +8,9 @@ public class CPTLiam {
 		String strCard3 = "";
 		String strCard4 = "";
 		String strCard5 = "";
+		String strCardType = "";
+		// aceClub, 2Club, 3Club, 4Club, 5Club, 6Club, 7Club, 8Club, 9Club, 10Club, jackClub, queenClub, kingClub
+		// aceDiamond, 2Diamond, 3Diamond, 4Diamond, 5Diamond, 6Diamond, 7Diamond, 8Diamond, 9Diamond, 10Diamond, jackDiamond, queenDiamond, kingDiamond
 		Console con = new Console();
 		con.println("Hello World!");
 	}
