bd02e5990ea3129a11b9ccc80c6c7535263baae3
Liam47a <Liam.JoycePlante26@ycdsbk12.ca>
2025-05-27 09:36:03 -0400

    Update CPTLiam.java

--- CPTLiam.java
+++ CPTLiam.java
@@ -3,16 +3,50 @@ import arc.*;
 public class CPTLiam {
 	
 	public static void main (String[] args) {
-		String strCard1 = "";
-		String strCard2 = "";
-		String strCard3 = "";
-		String strCard4 = "";
-		String strCard5 = "";
-		String strCardType = "";
-		// aceClub, 2Club, 3Club, 4Club, 5Club, 6Club, 7Club, 8Club, 9Club, 10Club, jackClub, queenClub, kingClub
-		// aceDiamond, 2Diamond, 3Diamond, 4Diamond, 5Diamond, 6Diamond, 7Diamond, 8Diamond, 9Diamond, 10Diamond, jackDiamond, queenDiamond, kingDiamond
+		int intSecretNum = 0;
+		intSecretNum = (int)(Math.random() * 100);
+		
+		int intGenNumber = 0;
+		int intGenSuit = 0;
+		
+		int intCardNumber = 0;
+		int intSuitNumber = 0;
+		
+		// 1st card stats
+		int intCard1Number = 0;
+		int intCard1Suit = 0;
+		// 2nd card stats
+		int intCard2Number = 0;
+		int intCard2Suit = 0;
+		// 3rd card stats
+		int intCard3Number = 0;
+		int intCard3Suit = 0;
+		// 4th card stats
+		int intCard4Number = 0;
+		int intCard4Suit = 0;
+		// 5th card stats
+		int intCard5Number = 0;
+		int intCard5Suit = 0;
+		
+		int intMoney = 1000;
+		
+		
+		/* Clubs (1)
+		aceClub, 2Club, 3Club, 4Club, 5Club, 6Club, 7Club, 8Club, 9Club, 10Club, 
+		jackClub, queenClub, kingClub
+		* Diamonds (2)
+		aceDiamond, 2Diamond, 3Diamond, 4Diamond, 5Diamond, 6Diamond, 7Diamond, 8Diamond, 
+		9Diamond, 10Diamond, jackDiamond, queenDiamond, kingDiamond
+		* Aces (3)
+		aceHeart, 2Heart, 3Heart, 4Heart, 5Heart, 6Heart, 7Heart, 8Heart, 9Heart, 10Heart, 
+		jackHeart, queenHeart, kingHeart
+		* Spades (4)
+		aceSpade, 2Spade, 3Spade, 4Spade, 5Spade, 6Spade, 7Spade, 8Spade, 9Spade, 10Spade, 
+		jackSpade, queenSpade, kingSpade
+		*/
 		Console con = new Console();
 		con.println("Hello World!");
 	}
 }
+		
 
