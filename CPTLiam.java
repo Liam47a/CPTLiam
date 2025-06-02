10c49a5c2ba3d2b1f00508a646554435c3a1e91e
Liam47a <Liam.JoycePlante26@ycdsbk12.ca>
2025-06-02 09:43:21 -0400

    More Functionality and Visuals (v0.15)
    Completed the basic card visuals.
    Added different stages to the game.
    The player can now lock cards to keep
    and can now shuffle the cards they do
    not want to keep once.

--- CPTLiam.java
+++ CPTLiam.java
@@ -17,22 +17,36 @@ public class CPTLiam {
 	// 1st card stats
 	static int intCard1Number = 0;
 	static int intCard1Suit = 1;
+	static boolean blnCard1Lock = false;
 	// 2nd card stats
 	static int intCard2Number = 0;
 	static int intCard2Suit = 1;
+	static boolean blnCard2Lock = false;
 	// 3rd card stats
 	static int intCard3Number = 0;
 	static int intCard3Suit = 1;
+	static boolean blnCard3Lock = false;
 	// 4th card stats
 	static int intCard4Number = 0;
 	static int intCard4Suit = 1;
+	static boolean blnCard4Lock = false;
 	// 5th card stats
 	static int intCard5Number = 0;
 	static int intCard5Suit = 1;
+	static boolean blnCard5Lock = false;
 	
 	static String strCardNumber = "";
 		
 	static int intMoney = 1000;
+	
+	static int intKeyPressed;
+	
+	// Tracks the stage of the game 
+	static int intGameStage = 0;
+	
+	// Allows for attributes to be applied only once when 
+	// the game stage changes and not constantly 
+	static boolean blnStageChange = false; 
 	
 	public static void main (String[] args) {
 		Console con = new Console(1920, 1080);
@@ -42,8 +56,68 @@ public class CPTLiam {
 		BufferedImage imgDiamond = con.loadImage("diamondIcon.png");
 		BufferedImage imgHeart = con.loadImage("heartIcon.png");
 		BufferedImage imgSpade = con.loadImage("spadeIcon.png");
-		// Core game loop 
+		cardUpdater(con);
+		// Core game loop
 		while (blnIsRunning == true) {
+			if (blnStageChange == true) {
+					con.setBackgroundColor(new Color(3, 80, 210));
+					blnStageChange = false;
+				}
+			if (intGameStage == 0) { // If cards not shuffled
+				con.drawString("Press Enter to Shuffle Cards", 500, 800);
+				con.drawString("Press the Number Keys to Lock-In Cards: (1, 2, 3, 4, 5)", 500, 900);
+			} if (intGameStage == 1) { // If cards not submitted
+				con.drawString("Press Enter to Submit Cards", 500, 800);
+			}
+			intKeyPressed = con.currentKey();
+			con.println(intKeyPressed);
+			if (intKeyPressed == 10) {
+				// If enter key pressed
+				if (intGameStage < 2) { // If no reshuffles have occured yet
+					// Enable the reshuffle process to start
+					intGameStage = intGameStage + 1;
+					blnStageChange = true;
+				}
+				if (intGameStage == 1) {
+					// Start the reshuffle, any unlocked cards will be shuffled
+					// Locked cards will stay the same
+					if (blnCard1Lock == false) {
+						intCard1Number = 0; 
+					} if (blnCard2Lock == false) {
+						intCard2Number = 0;
+					} if (blnCard3Lock == false) {
+						intCard3Number = 0;
+					} if (blnCard4Lock == false) {
+						intCard4Number = 0;
+					} if (blnCard5Lock == false) {
+						intCard5Number = 0;
+					}
+				}
+				if (intGameStage == 2) {
+					// Final stage of the game, will now check what card 
+					// combinations the player has and will reward them accordingly
+				}
+				cardUpdater(con);
+			}
+			if (intKeyPressed == 49) {
+				// If key 1 pressed, lock Card 1
+				blnCard1Lock = true;
+			}
+			if (intKeyPressed == 50) {
+				// If key 2 pressed, lock Card 2
+				blnCard2Lock = true;
+			}
+			if (intKeyPressed == 51) {
+				// If key 3 pressed, lock Card 3
+				blnCard3Lock = true;
+			}
+			if (intKeyPressed == 52) {
+				// If key 4 pressed, lock Card 4
+				blnCard4Lock = true;
+			}
+			if (intKeyPressed == 53) {
+				// If key 5 pressed, lock Card 5 
+			}
 			while (intCard1Number == 0 || intCard2Number == 0 || intCard3Number == 0 || intCard4Number == 0 || intCard5Number == 0) {
 				cardUpdater(con);
 			}
@@ -67,16 +141,16 @@ public class CPTLiam {
 			
 			con.setDrawColor(new Color(0, 0, 0));
 			if (intCard1Number == 1) { // Ace
-				con.drawString("A", 550, 525);
+				con.drawString("A", 555, 525);
 			} else if (intCard1Number == 11) { // Jack
-				con.drawString("J", 550, 525);
+				con.drawString("J", 555, 525);
 			} else if (intCard1Number == 12) { // Queen
-				con.drawString("Q", 550, 525);
+				con.drawString("Q", 555, 525);
 			} else if (intCard1Number == 13) { // King
-				con.drawString("K", 550, 525);
+				con.drawString("K", 555, 525);
 			} else { // Numbers 2-10 
 				strCardNumber = Integer.toString(intCard1Number);
-				con.drawString(strCardNumber, 550, 525);
+				con.drawString(strCardNumber, 555, 525);
 			}
 			// Card 2
 			con.setDrawColor(new Color(255, 255, 255));
@@ -118,7 +192,25 @@ public class CPTLiam {
 			} else if (intCard3Suit == 4) {
 				con.drawImage(imgSpade, 810, 500);
 			}
+			
+			// Number Visuals (+222)
+			
+			con.setDrawColor(new Color(0, 0, 0));
+			if (intCard3Number == 1) { // Ace
+				con.drawString("A", 994, 525);
+			} else if (intCard3Number == 11) { // Jack
+				con.drawString("J", 994, 525);
+			} else if (intCard3Number == 12) { // Queen
+				con.drawString("Q", 994, 525);
+			} else if (intCard3Number == 13) { // King
+				con.drawString("K", 994, 525);
+			} else { // Numbers 2-10 
+				strCardNumber = Integer.toString(intCard3Number);
+				con.drawString(strCardNumber, 994, 525);
+			}
+			
 			// Card 4
+			con.setDrawColor(new Color(255, 255, 255));
 			con.fillRect(1070, 510, 180, 230);
 			if (intCard4Suit == 1) {
 				con.drawImage(imgClub, 1030, 500);
@@ -129,7 +221,25 @@ public class CPTLiam {
 			} else if (intCard4Suit == 4) {
 				con.drawImage(imgSpade, 1030, 500);
 			}
+			
+			// Number Visuals (+222)
+			
+			con.setDrawColor(new Color(0, 0, 0));
+			if (intCard4Number == 1) { // Ace
+				con.drawString("A", 1216, 525);
+			} else if (intCard4Number == 11) { // Jack
+				con.drawString("J", 1216, 525);
+			} else if (intCard4Number == 12) { // Queen
+				con.drawString("Q", 1216, 525);
+			} else if (intCard4Number == 13) { // King
+				con.drawString("K", 1216, 525);
+			} else { // Numbers 2-10 
+				strCardNumber = Integer.toString(intCard4Number);
+				con.drawString(strCardNumber, 1216, 525);
+			}
+			
 			// Card 5
+			con.setDrawColor(new Color(255, 255, 255));
 			con.fillRect(1290, 510, 180, 230);
 			if (intCard5Suit == 1) {
 				con.drawImage(imgClub, 1250, 500);
@@ -140,9 +250,26 @@ public class CPTLiam {
 			} else if (intCard5Suit == 4) {
 				con.drawImage(imgSpade, 1250, 500);
 			}
+			
+			// Number Visuals (+222)
+			
+			con.setDrawColor(new Color(0, 0, 0));
+			if (intCard5Number == 1) { // Ace
+				con.drawString("A", 1438, 525);
+			} else if (intCard5Number == 11) { // Jack
+				con.drawString("J", 1438, 525);
+			} else if (intCard5Number == 12) { // Queen
+				con.drawString("Q", 1438, 525);
+			} else if (intCard5Number == 13) { // King
+				con.drawString("K", 1438, 525);
+			} else { // Numbers 2-10 
+				strCardNumber = Integer.toString(intCard5Number);
+				con.drawString(strCardNumber, 1438, 525);
+			}
+			
 			// [-----]
 			con.repaint();
-			con.sleep(100);
+			con.sleep(50);
 		
 		}
 	}
@@ -157,20 +284,19 @@ public class CPTLiam {
 			nGen(con);
 			intCard1Number = intGenNumber;
 			intCard1Suit = intGenSuit;
-			System.out.println(intCard1Suit);
-		} else if (intCard2Number == 0) {
+		} if (intCard2Number == 0) {
 			nGen(con);
 			intCard2Number = intGenNumber;
 			intCard2Suit = intGenSuit;
-		} else if (intCard3Number == 0) {
+		} if (intCard3Number == 0) {
 			nGen(con);
 			intCard3Number = intGenNumber;
 			intCard3Suit = intGenSuit;
-		} else if (intCard4Number == 0) {
+		} if (intCard4Number == 0) {
 			nGen(con);
 			intCard4Number = intGenNumber;
 			intCard4Suit = intGenSuit;
-		} else if (intCard5Number == 0) {
+		} if (intCard5Number == 0) {
 			nGen(con);
 			intCard5Number = intGenNumber;
 			intCard5Suit = intGenSuit;
